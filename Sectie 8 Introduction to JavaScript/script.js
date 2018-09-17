21eab66ad5b087cf12d349ae63a0261949f55d3e
Rianne <marchandrianne@gmail.com>
2018-09-17 16:51:54 +0200

    prompt, alert and console.log intro

---
+++ Sectie 8 Introduction to JavaScript/script.js
@@ -0,0 +1,6 @@
+// 3 methods: prompt, alert and console.log	
+
+
+var userName = prompt("What is your name?");
+alert("Nice to meet you, " + userName);
+console.log("Also great to meet you, " + userName);
