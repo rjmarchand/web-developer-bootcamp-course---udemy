48b7c97b824d6f02f33d4c0256d950d90f974db8
Rianne <marchandrianne@gmail.com>
2018-09-24 16:31:57 +0200

    arrays intro opdracht

---
+++ Sectie 11 JavaScript Basics; Arrays/Opdrachten/arrayExercises.js
@@ -0,0 +1,18 @@
+var numbers = [22, 67, 33, 96, 88];
+
+// What does the following line print?
+console.log(numbers[numbers.length])
+
+// undefined, because numbers[5] does not exist
+
+
+var friendGroups = [
+	["Harry", "Ron", "Hermione"],
+	["Malfoy", "Crabbe", "Goyle"],
+	["Mooney", "Wormtail", "Prongs"]
+];
+
+// What is the result of this line?
+console.log(friendGroups[2][0]);
+
+// Mooney, it takes [2] array (which is the 3rd nested array) and then [0], which is the first item (Mooney)
