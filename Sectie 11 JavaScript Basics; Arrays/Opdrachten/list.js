a133fa62cf772c2606480f783bf435041ae05c2f
Rianne <marchandrianne@gmail.com>
2018-10-01 11:00:26 +0200

    update list exercise

--- Sectie 11 JavaScript Basics; Arrays/Opdrachten/list.js
+++ Sectie 11 JavaScript Basics; Arrays/Opdrachten/list.js
@@ -1,4 +1,5 @@
 window.setTimeout(function() {
+
 	var todos = ["Buy new turtle"];
 
 	var input = prompt("What would you like to do?");
@@ -6,13 +7,35 @@ window.setTimeout(function() {
 	while(input !== "quit"){
 
 		if(input === "list"){
-			console.log(todos);
+			listTodos();
 		}
 		else if(input === "new"){
-			var newTodo = prompt("Enter new todo");
-			todos.push(newTodo);
+			newTodo();
+		}
+		else if(input === "delete"){
+			deleteTodo();
 		}	
 		input = prompt("What would you like to do?");	
 	}
 	console.log("Ok, you quit the app");
+
+
+function listTodos(){
+	console.log("********************");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});			
+	console.log("********************");
+}
+function newTodo(){
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+	console.log("Succesfully added new todo");
+}
+function deleteTodo(){
+	var index = prompt("Enter index of todo to delete");
+	todos.splice(index,1);
+	console.log("Succesfully deleted todo " + index);
+}
+
 }, 500);
