b64b7bdc7d382b6259f190b8f67c5eeec62135bb
rjmarchand <marchandrianne@gmail.com>
2018-10-14 19:22:50 +0200

    objects exercise

---
+++ Sectie 12 JavaScript Basics; Objects/objectsExercise.js
@@ -0,0 +1,17 @@
+var someObject = {
+	friends: [
+		{name: "Malfoy"},
+		{name: "Crabbe"},
+		{name: "Goyle"}
+	],
+	color: "baby blue",
+	isEvil: true
+};
+
+// Write code to retrieve "Malfoy" from someObject
+// Go one layer at a time!
+
+
+someObject.friends
+someObject.friends[0]
+someObject.friends[0].name
