f2755339ab0ac401b34f73d0c4ad612496203ea4
rjmarchand <marchandrianne@gmail.com>
2018-11-01 16:14:36 +0100

    Finished js

--- Sectie 20 Todo List app/assets/js/todos.js
+++ Sectie 20 Todo List app/assets/js/todos.js
@@ -1,3 +1,23 @@
-$("li").click(function(){
+// Check off specific todos by clicking
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
+});
+
+// Click on X to delete todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// Create a new todo
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// Grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+	}
 });
